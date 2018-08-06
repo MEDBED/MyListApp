0686005ac8d11b2f8f98f4a6bc3e85c53533e230
MEDBED <med.bouhamed@live.fr>
2018-08-06 16:51:19 +0200

    Last Version

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,7 +9,11 @@ export class SidebarComponent implements OnInit {
   constructor(private router : Router) {
     router
       .events
-      .subscribe((_ : NavigationEnd) => this.currentUrl = _.url);
+      .subscribe((_ : NavigationEnd) => {
+        if (!!_.url) {
+          this.currentUrl = _.url
+        }
+      });
   }
 
   ngOnInit() {}
