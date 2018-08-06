8811466ae1f4d9e902ab9a1febde50f490d4e8df
MEDBED <med.bouhamed@live.fr>
2018-08-06 14:12:42 +0200

    service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+//import our components
+
+import {UsersComponent} from './users/users.component';
+import {DetailsComponent} from './details/details.component';
+import {PostsComponent} from './posts/posts.component';
+
+//This were to define all of our routes
+
+const routes : Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  }, {
+    path: 'details/:id',
+    component: DetailsComponent
+  }, {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
